0812b416d3780f839920e9a896f4c38a48e63ba4
kanwar sandhu <kanwarsandhu1809@gmail.com>
2021-01-21 05:04:00 +0530

    added code.py

--- code.py
+++ code.py
@@ -1,2 +1,13 @@
 def are_valid_groups(a, b):
+    for i in a:
+        indicate = 0
+        for j in b:
+            if i in j:
+                indicate = 1
+                break
+        if indicate == 0:
+            return False
+
+    return True
+
 	
