d3d3d18217f10e25e4e4257c81e1734c788df6c2
waraich1 <waraich@mcmaster.ca>
2021-01-20 19:01:48 -0500

    Player

--- code.py
+++ code.py
@@ -1,10 +1,10 @@
 def are_valid_groups2(a, b):
     x = set(a)
-    if (len(x) != len(a)):
+    if (len(x)!= len(a)):
         return False
 
-    for i in b:
-        if (len(i) != 2) and (len(i) != 3):
+    for i in range(len(b)):
+        if (len(b[i]) != 2) and (len(b[i]) != 3):
             return False
 
     return True
